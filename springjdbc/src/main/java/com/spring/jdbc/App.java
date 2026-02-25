39880781b00665770ce80f132b58a8ef3f544234
Dharmeshwarkumarpriy <dkpriy8402@gmail.com>
2026-02-25 15:54:45 +0530

    Add... Inserting data to database using spring JDBC.

--- springjdbc/src/main/java/com/spring/jdbc/App.java
+++ springjdbc/src/main/java/com/spring/jdbc/App.java
@@ -4,10 +4,9 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-/**
- * Hello world!
- *
- */
+import com.spring.jdbc.dao.StudentDao;
+import com.spring.jdbc.enties.Student;
+
 public class App {
 	public static void main(String[] args) {
 		System.out.println("my program started............");
@@ -15,12 +14,21 @@ public class App {
 		ApplicationContext context = new ClassPathXmlApplicationContext("com/spring/jdbc/config.xml");
 		JdbcTemplate template = context.getBean("jdbcTemplate", JdbcTemplate.class);
 
-		// insert query.....
-		String query = "insert into student(id,name,city) values(?,?,?)";
+//		// insert query.....
+//		String query = "insert into student(id,name,city) values(?,?,?)";
+//
+//		// fire query....
+//		int result = template.update(query, 42585, "uttam", "bihar");
+//		System.out.println("number of record inserted.." + result);
 
-		// fire query....
-		int result = template.update(query, 42585, "uttam", "bihar");
-		System.out.println("number of record inserted.." + result);
-
+		StudentDao studentDao = context.getBean("studentDao",StudentDao.class);
+		Student student=new Student();
+		student.setId(5656);
+		student.setName("priye");
+		student.setCity("rohtas");
+		
+		int result = studentDao.insert(student);
+		System.out.println("student added:" + result);
+		
 	}
 }
---
+++ springjdbc/src/main/java/com/spring/jdbc/dao/StudentDao.java
@@ -0,0 +1,9 @@
+package com.spring.jdbc.dao;
+
+import com.spring.jdbc.enties.Student;
+
+public interface StudentDao {
+
+	public int insert(Student student);
+	
+}
