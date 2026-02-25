3a8de16db571222555e269e77a897a18ff4d4a0b
Dharmeshwarkumarpriy <dkpriy8402@gmail.com>
2026-02-25 13:39:58 +0530

    Add... spring JDBC  Database connection using XML File and add data in database(springjdbc(id,name,city)).

---
+++ springjdbc/src/main/java/com/spring/jdbc/App.java
@@ -0,0 +1,26 @@
+package com.spring.jdbc;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("my program started............");
+		// spring jdbc=> JdbcTemplalte
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/spring/jdbc/config.xml");
+		JdbcTemplate template = context.getBean("jdbcTemplate", JdbcTemplate.class);
+
+		// insert query.....
+		String query = "insert into student(id,name,city) values(?,?,?)";
+
+		// fire query....
+		int result = template.update(query, 42585, "uttam", "bihar");
+		System.out.println("number of record inserted.." + result);
+
+	}
+}
