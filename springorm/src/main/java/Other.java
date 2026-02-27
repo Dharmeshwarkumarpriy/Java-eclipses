5ad96af4cabd6a47c3ed1bde2b0e16c2693cdc39
Dharmeshwarkumarpriy <dkpriy8402@gmail.com>
2026-02-27 22:11:31 +0530

    Spring ORM add dependencies and configuration file, entity, dao, config file.

---
+++ springorm/src/main/java/Other.java
@@ -0,0 +1,34 @@
+//
+//public class Other {
+//	<!-- Source: https://mvnrepository.com/artifact/org.springframework/spring-context -->
+//		<dependency>
+//			<groupId>org.springframework</groupId>
+//			<artifactId>spring-context</artifactId>
+//			<version>6.1.20</version>
+//			<scope>compile</scope>
+//		</dependency>
+//
+//		<!-- Source: https://mvnrepository.com/artifact/org.springframework/spring-orm -->
+//		<dependency>
+//			<groupId>org.springframework</groupId>
+//			<artifactId>spring-orm</artifactId>
+//			<version>6.1.20</version>
+//			<scope>compile</scope>
+//		</dependency>
+//
+//		<dependency>
+//			<groupId>mysql</groupId>
+//			<artifactId>mysql-connector-java</artifactId>
+//			<version>8.0.31</version>
+//			<scope>compile</scope>
+//		</dependency>
+//
+//
+//		<!-- Source: https://mvnrepository.com/artifact/org.hibernate.orm/hibernate-core -->
+//<dependency>
+//    <groupId>org.hibernate.orm</groupId>
+//    <artifactId>hibernate-core</artifactId>
+//    <version>6.2.47.Final</version>
+//    <scope>compile</scope>
+//</dependency>
+//}
