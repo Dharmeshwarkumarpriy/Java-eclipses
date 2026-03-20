ce8cadd6aed3673930ea9e731dff8f6d4f40b606
Dharmeshwarkumarpriy <dkpriy8402@gmail.com>
2026-03-20 16:05:45 +0530

    redirect in spring mvc 1.HttpServletResponse
    2.redirect prefix via...
     public string handler(){
    return "redirect:/url"
    }
    
    3.RedirectView...
    public RedirectView handler(){
    RedirectView r=new RedirectView();
    r.setUrl();
    return r;
    }

--- mvc/src/main/java/mvc/controller/ReController.java
+++ mvc/src/main/java/mvc/controller/ReController.java
@@ -2,15 +2,18 @@ package mvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class ReController {
 
 	@RequestMapping("/one")
-	public String one() {
+	public RedirectView one() {
 		
 		System.out.println("this is one handler");
-		return "redirect:/two";
+		RedirectView redirectView =new RedirectView();
+		redirectView.setUrl("two");
+		return redirectView;
 	}
 	
 	@RequestMapping("/two")
