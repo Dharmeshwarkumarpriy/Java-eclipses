f8d3499f17f4140a6a6188478f73be0e8a5490dc
Dharmeshwarkumarpriy <dkpriy8402@gmail.com>
2026-03-20 15:17:26 +0530

    return Redirect in spring MVC.

---
+++ mvc/src/main/java/mvc/controller/ReController.java
@@ -0,0 +1,23 @@
+package mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ReController {
+
+	@RequestMapping("/one")
+	public String one() {
+		
+		System.out.println("this is one handler");
+		return "redirect:/two";
+	}
+	
+	@RequestMapping("/two")
+	public String two() {
+		
+		System.out.println("this is two handler");
+		return "contact";
+	}
+	
+}
