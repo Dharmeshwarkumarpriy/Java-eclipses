0e50e07d99ba6ac89bebad22ff2907adca1fcd86
Dharmeshwarkumarpriy <dkpriy8402@gmail.com>
2026-04-03 21:01:22 +0530

    spring boot project demo(springbootproject) jdk11, tomcat9.

---
+++ springbootproject/src/main/java/com/dharam/springbootproject/SpringBootProjectAppliction.java
@@ -0,0 +1,17 @@
+package com.dharam.springbootproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@Configuration
+//@EnableAutoConfiguration
+//@ComponentScan
+
+public class SpringBootProjectAppliction {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProjectAppliction.class, args);
+	}
+	
+}
